d1322ff2648cd69a97e1c598a504ee8676b1649a
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-04-24 00:27:50 +0200

    Делает наследование для классов, требующих переопределения конструктора

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,5 +1,6 @@
+import AbstractComponent from "../abstract-component.js";
 import {MONTH_NAMES} from "../../helpers/constants.js";
-import {createElement, formatTime} from "../../helpers/utils.js";
+import {formatTime} from "../../helpers/utils.js";
 
 const createTaskTemplate = (task) => {
   const {description, dueDate, color, repeatingDays, isArchive, isFavorite} = task;
@@ -60,26 +61,13 @@ const createTaskTemplate = (task) => {
           </article>`;
 };
 
-export default class Task {
+export default class Task extends AbstractComponent {
   constructor(task) {
+    super();
     this._task = task;
-
-    this._element = null;
   }
 
   getTemplate() {
     return createTaskTemplate(this._task);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
