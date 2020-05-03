0b04c382e5725278543a75c30eea813e4374f019
Tina Kuzmenko <tina.kuzmenko@gmail.com>
2020-05-03 13:09:03 +0200

    Создает контроллер для отрисовки таски. WIP

--- src/controllers/board.js
+++ src/controllers/board.js
@@ -1,56 +1,13 @@
-import TaskComponent from "../components/task/task.js";
-import TaskEditComponent from "../components/task/task-edit.js";
 import ButtonLoadMoreComponent from "../components/button-load-more/button-load-more.js";
 import NoTasksComponent from "../components/task/no-tasks.js";
 import SortComponent from "../components/sort/sort.js";
 import TasksComponent from "../components/task/tasks.js";
-
-import {Keycode} from "../helpers/constants.js";
+import TaskController from "./task.js";
 import {getSortedTasks} from "../helpers/utils.js";
-import {render, replace, remove} from "../helpers/render.js";
+import {render, remove} from "../helpers/render.js";
 
 const SHOWING_TASKS_COUNT_ON_START = 8;
 const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
-
-const renderTask = (taskListElement, task) => {
-  const replaceTaskToEdit = () => {
-    replace(taskEditComponent, taskComponent);
-  };
-
-  const replaceEditToTask = () => {
-    replace(taskComponent, taskEditComponent);
-  };
-
-  const onEscKeyDown = (evt) => {
-    if (evt.key === Keycode.ESCAPE) {
-      replaceEditToTask();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const taskComponent = new TaskComponent(task);
-  const taskEditComponent = new TaskEditComponent(task);
-
-  taskComponent.setEditButtonClickHandler(() => {
-    replaceTaskToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  taskEditComponent.setSubmitHandler((evt) => {
-    evt.preventDefault();
-    replaceEditToTask();
-    document.removeEventListener(`keydown`, onEscKeyDown);
-  });
-
-  render(taskListElement, taskComponent);
-};
-
-const renderTasksOnBoard = (tasksArray, tasksStartCount, tasksEndCount, tasksBoard) => {
-  tasksArray.slice(tasksStartCount, tasksEndCount)
-      .forEach((task) => {
-        renderTask(tasksBoard, task);
-      });
-};
 
 export default class BoardController {
   constructor(container) {
@@ -59,6 +16,13 @@ export default class BoardController {
     this._sortComponent = new SortComponent();
     this._tasksComponent = new TasksComponent();
     this._loadMoreButtonComponent = new ButtonLoadMoreComponent();
+  }
+
+  renderTasksOnBoard(tasks, tasksStartCount, tasksEndCount, tasksBoard) {
+    tasks.slice(tasksStartCount, tasksEndCount)
+        .forEach((task) => {
+          renderTask(tasksBoard, task);
+        });
   }
 
   renderButtonLoadMore(tasks, showingTasksCount, taskListElement) {
@@ -72,7 +36,7 @@ export default class BoardController {
       const prevTasksCount = showingTasksCount;
       showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
 
-      renderTasksOnBoard(tasks, prevTasksCount, showingTasksCount, taskListElement);
+      this.renderTasksOnBoard(tasks, prevTasksCount, showingTasksCount, taskListElement);
 
       if (showingTasksCount >= tasks.length) {
         remove(this._loadMoreButtonComponent);
@@ -96,7 +60,7 @@ export default class BoardController {
 
     let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
 
-    renderTasksOnBoard(tasks, 0, showingTasksCount, taskListElement);
+    this.renderTasksOnBoard(tasks, 0, showingTasksCount, taskListElement);
     this.renderButtonLoadMore(tasks, showingTasksCount, taskListElement);
 
     this._sortComponent.setSortTypeChangeHandler((sortType) => {
@@ -105,7 +69,7 @@ export default class BoardController {
       const sortedTasks = getSortedTasks(tasks, sortType);
       taskListElement.innerHTML = ``;
 
-      renderTasksOnBoard(sortedTasks, 0, showingTasksCount, taskListElement);
+      this.renderTasksOnBoard(sortedTasks, 0, showingTasksCount, taskListElement);
       this.renderButtonLoadMore(sortedTasks, showingTasksCount, taskListElement);
     });
   }
---
+++ src/controllers/task.js
@@ -0,0 +1,48 @@
+import TaskComponent from "../components/task/task.js";
+import TaskEditComponent from "../components/task/task-edit.js";
+import {Keycode} from "../helpers/constants.js";
+import {render, replace} from "../helpers/render.js";
+
+export default class TaskController {
+  constructor(container) {
+    this._container = container;
+
+    this._taskComponent = null;
+    this._taskEditComponent = null;
+
+    this._onEscKeyDown = this._onEscKeyDown.bind(this);
+  }
+
+  render(task) {
+    this._taskComponent = new TaskComponent(task);
+    this._taskEditComponent = new TaskEditComponent(task);
+
+    this._taskComponent.setEditButtonClickHandler(() => {
+      this._replaceTaskToEdit();
+      document.addEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    this._taskEditComponent.setSubmitHandler((evt) => {
+      evt.preventDefault();
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    });
+
+    render(this._container, this._taskComponent);
+  }
+
+  _replaceTaskToEdit() {
+    replace(this._taskEditComponent, this._taskComponent);
+  }
+
+  _replaceEditToTask() {
+    replace(this._taskComponent, this._taskEditComponent);
+  }
+
+  _onEscKeyDown(evt) {
+    if (evt.key === Keycode.ESCAPE) {
+      this._replaceEditToTask();
+      document.removeEventListener(`keydown`, this._onEscKeyDown);
+    }
+  }
+}
